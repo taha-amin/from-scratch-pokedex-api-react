e489997a9ab2fae6e4300155a39dda2ee0316015
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-16 14:20:05 -0700

    wrote app.js to show how web app will be shown to user and has search bar for user to search through api data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getPokemons } from './services/fetch-utils';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [query, setQuery] = useState('char');
+
+  async function load() {
+    const {
+      data: { results },
+    } = await getPokemons(query);
+
+    setPokemons(results);
+  }
+
+  useEffect(() => {
+    load();
+  }, []); //eslint-disable-line
+
+  async function handleSearch(e) {
+    e.preventDefault();
+
+    load();
+  }
+
   return (
     <div className="App">
+      <form onSubmit={handleSearch}>
+        <input onChange={(e) => setQuery(e.target.value)} />
+        <button>Search</button>
+      </form>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {pokemons.map(({ pokemon, generation_id, url_image }, i) => (
+          <div key={pokemon + i}>
+            <h2>{pokemon}</h2>
+            <p>{generation_id}</p>
+            <img src={`http://assets.pokemon.com/assets/cms2/img/pokedex/full/${url_image}`} />
+          </div>
+        ))}
       </header>
     </div>
   );
