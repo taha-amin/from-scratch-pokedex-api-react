1ab593c0837da71ab614b0567585bd5f788a7d35
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-16 14:03:22 -0700

    changed fetch in fetch-utils.js

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,5 +1,5 @@
 export async function getPokemons(searchFilter) {
-  const rawResponse = await fetch(`/.netlify/functions/movies?searchFilter=${searchFilter}`);
+  const rawResponse = await fetch(`/.netlify/functions/pokemons?searchFilter=${searchFilter}`);
   const data = await rawResponse.json();
 
   return data;
