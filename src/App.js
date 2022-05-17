b364dc187aa520e491f3b5d6f07eebd4ef04327c
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-05-16 17:09:00 -0700

    imported spinner from respective file, , set state for isLoading, called state inside our load function, first one true so before data is grabbed spinner shows, then after data is grabbed spinner is gone. In return wrote ternary that if isloading is true, show spinner, if false show data being mapped

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getPokemons } from './services/fetch-utils';
+import Spinner from './Spinner';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [query, setQuery] = useState('a');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function load() {
+    setIsLoading(true);
+
     const {
       data: { results },
     } = await getPokemons(query);
+
+    setIsLoading(false);
 
     setPokemons(results);
   }
@@ -31,14 +37,18 @@ function App() {
         <button>Search</button>
       </form>
       <header className="App-header">
-        {pokemons.map(({ pokemon, height, weight, url_image }, i) => (
-          <div key={pokemon + i}>
-            <h2>Name: {pokemon}</h2>
-            <p>Height: {height}</p>
-            <p>Weight: {weight}</p>
-            <img src={url_image} />
-          </div>
-        ))}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          pokemons.map(({ pokemon, height, weight, url_image }, i) => (
+            <div key={pokemon + i}>
+              <h2>Name: {pokemon}</h2>
+              <p>Height: {height}</p>
+              <p>Weight: {weight}</p>
+              <img src={url_image} />
+            </div>
+          ))
+        )}
       </header>
     </div>
   );
